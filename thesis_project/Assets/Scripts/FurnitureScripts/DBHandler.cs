801911920363fa3a2daf1485ddd9ec5ae2265083
Carlos Pinto <carlosnpinto2@gmail.com>
2015-04-25 16:10:02 +0100

    updated grab and drop object system

--- thesis_project/Assets/Scripts/FurnitureScripts/DBHandler.cs
+++ thesis_project/Assets/Scripts/FurnitureScripts/DBHandler.cs
@@ -1,27 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-public class DBHandler{
-
-	private static DBHandler instance = null;
-
-	private DBHandler() {
-
-	}
-
-	public static DBHandler Instance {
-		get {
-			if(instance == null) {
-				instance = new DBHandler();
-			}
-			return instance;
-		}
-	}
-
-	public Furniture[] GetFurnitures () {
-		Furniture[] furnitures;
-		furnitures = new Furniture[1];
-		furnitures[0] = new Furniture(1, "Cubo", "floor", "default characteristics", "200€");
-		return furnitures;
-	}
-}
+﻿using System.Collections;using System.Collections.Generic;public class DBHandler{		private static DBHandler instance = null;		private DBHandler() {			}		public static DBHandler Instance {		get {			if(instance == null) {				instance = new DBHandler();			}			return instance;		}	}		public Furniture[] GetFurnitures () {		Furniture[] furnitures;		furnitures = new Furniture[1];		furnitures[0] = new Furniture(1, "imageURL", "Cubo", "floor", "default description", "200€", 100);		return furnitures;	}}
--- thesis_project/Assets/Scripts/FurnitureScripts/FurnitureHandler.cs
+++ thesis_project/Assets/Scripts/FurnitureScripts/FurnitureHandler.cs
@@ -1,52 +1,1 @@
-using UnityEngine;
-using System.Collections;
-
-public class FurnitureHandler : MonoBehaviour {
-
-	private Furniture furniture;
-	private bool isGrabbed = false;
-
-	//Temporary
-	void Start () {
-		EditorGameManager egm = EditorGameManager.Instance;
-		Furniture furniture_temp = egm.Furnitures [0];
-		SetFurniture(furniture_temp);
-	}
-
-	//must be the first call to this script
-	public void SetFurniture (Furniture _furniture_) {
-		furniture = _furniture_;
-	}
-
-	public Transform GrabFurniture() {
-		Debug.Log("Grab object "+furniture.Name);
-
-		isGrabbed = true;
-
-		this.gameObject.transform.GetComponent<Rigidbody>().useGravity=false;
-		this.gameObject.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-
-		return gameObject.transform;
-	}
-
-	public bool DropFurniture () {
-		Debug.Log("Drop Object "+furniture.Name);
-
-		//check furniture type to check where furniture can be dropped or attached to
-		//if(furniture.Type)....
-
-		this.gameObject.GetComponent<Rigidbody>().useGravity=true;
-		this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-
-		isGrabbed = false;
-
-		return true;
-	}
-
-	void Update() {
-		if(isGrabbed) {
-			this.gameObject.transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
-			this.gameObject.transform.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-		}
-	}
-}
+using UnityEngine;using System.Collections;public class FurnitureHandler : MonoBehaviour {		private Furniture furniture;	private EditorGameManager egm;		void Start () {		egm = EditorGameManager.Instance;		//Temporary		Furniture furniture_temp = egm.Furnitures [0];		SetFurniture(furniture_temp);		this.gameObject.GetComponent<Rigidbody> ().mass = furniture.Mass;	}		//must be the first call to this script	public void SetFurniture (Furniture _furniture_) {		furniture = _furniture_;	}		public Transform GrabFurniture(Transform newParent) {		Debug.Log("Grab object "+furniture.Name);				this.transform.SetParent (newParent);				egm.IsGrabbingObject = true;		egm.GrabbedObject = this.gameObject;				this.gameObject.transform.GetComponent<Rigidbody>().useGravity=false;		this.gameObject.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;				return gameObject.transform;	}		public void DropFurniture () {		Debug.Log("Drop Object "+furniture.Name);				//		check furniture type to check where furniture can be dropped or attached to		//		if(furniture.Type)....				this.transform.SetParent (null);				this.gameObject.GetComponent<Rigidbody>().useGravity=true;		this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;				egm.IsGrabbingObject = false;		egm.GrabbedObject = null;	}		//	void onCollisionEnter(Collision collision) {	//		collision.gameObject.transform.parent = null; //drop object on collision	//	}		void Update() {		if (egm.GrabbedObject == this.gameObject) {			this.gameObject.transform.GetComponent<Rigidbody> ().velocity = Vector3.zero;			this.gameObject.transform.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;		}	}}
