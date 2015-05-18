38e68a23e6384f4f4c9b5c2b490aa9bb529df64e
Carlos Pinto <carlosnpinto2@gmail.com>
2015-05-18 22:59:24 +0100

    added collision detection and transparent walls while moving objects to ease editing

---
+++ thesis_project/Assets/Scripts/FurnitureScripts/WallCollision.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCollision : MonoBehaviour {
+	private EditorGameManager egm;
+	private MenuManager mm;
+
+	void Start() {
+		egm = EditorGameManager.Instance;
+		mm = MenuManager.Instance;
+	}
+
+	void Update() {
+		if (mm.ActivatedMenu == MenuManager.Menu.none) {
+			if (egm.GrabbedObject != null) {
+				foreach (GameObject wall in GameObject.FindGameObjectsWithTag("wall")) {
+					Renderer rend = wall.GetComponent<Renderer> ();
+					rend.material.shader = Shader.Find ("Particles/Additive (Soft)");
+					wall.GetComponent<BoxCollider> ().enabled = false;
+				}
+			} else {
+				foreach (GameObject wall in GameObject.FindGameObjectsWithTag("wall")) {
+					Renderer rend = wall.GetComponent<Renderer> ();
+					rend.material.shader = Shader.Find ("Standard");
+					wall.GetComponent<BoxCollider> ().enabled = true;
+				}
+			}
+		}
+	}
+}
