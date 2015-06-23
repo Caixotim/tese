9631f9c42491ac18356ff3779a8001a4205083ba
Carlos Pinto <carlosnpinto2@gmail.com>
2015-06-23 13:59:05 +0100

    Implemented trigger to change furniture name (know if it is outside base). Implemented player height.

--- thesis_project/Assets/Scripts/FurnitureScripts/TriggerFurnitureBase.cs
+++ thesis_project/Assets/Scripts/FurnitureScripts/TriggerFurnitureBase.cs
@@ -1,23 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TriggerName : MonoBehaviour {
+public class TriggerFurnitureBase : MonoBehaviour {
 
 	void OnTriggerExit(Collider other) {
 		GameObject enteredGameObject = other.gameObject;
-		Debug.Log("Exit");
-		Debug.Log(enteredGameObject.name);
 		if(enteredGameObject.tag.Equals("furniture") && enteredGameObject.name.EndsWith("selection")) {
 			string[] newName = enteredGameObject.name.Split("_"[0]);
 			enteredGameObject.name = newName[0];
 		}
-	}
-
-	void OnTriggerEnter(Collider other) {
-		Debug.Log("entered trigger");
-	}
-
-	void OnTriggerStay(Collider other) {
-		Debug.Log(other.gameObject.name + " is inside furniture base!");
 	}
 }
