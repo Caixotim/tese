302cae030019740b81855c28539a4b1b7f4389de
Carlos Pinto <carlosnpinto2@gmail.com>
2015-06-24 23:04:48 +0100

    Implemented initial delete menu.

---
+++ thesis_project/Assets/Scripts/Misc/DeleteTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider coll) {
+		if(coll.gameObject.tag == "furniture") {
+			FurnitureHandler fh = coll.gameObject.GetComponent<FurnitureHandler>();
+			fh.DropFurniture();
+			Destroy(coll.gameObject);
+		}
+	}
+}
