2c550057c5cb0fd1ffc76f097b4cc76ac2d64fe1
Carlos Pinto <carlosnpinto2@gmail.com>
2015-07-04 11:10:50 +0100

    Fixed player bouncing by removing character controller

---
+++ thesis_project/Assets/Scripts/Player/PlayerFixes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFixes : MonoBehaviour {
+
+	private SimulationManager sm;
+
+	void Start () {
+		sm = SimulationManager.Instance;
+	}
+	
+	void Update () {
+		LevelPlayerHeight();
+		ResetPlayerVelocity();
+	}
+
+	private void LevelPlayerHeight () {
+		RaycastHit hit;
+		Ray ray = new Ray ();
+		ray.origin = this.transform.position;
+		ray.direction = -Vector3.up;
+		if (Physics.Raycast (ray, out hit, 1.0f)) {
+			 if(hit.transform.tag.Equals ("floor")) {
+			 	Transform floorObj = hit.transform;
+				this.transform.position = new Vector3 (this.transform.position.x, hit.transform.position.y + sm.UserHeight, this.transform.position.z);
+			}
+		}
+	}
+
+	private void ResetPlayerVelocity () {
+		this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+	}
+}
