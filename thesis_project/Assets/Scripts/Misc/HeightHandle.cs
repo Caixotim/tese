9d858869a760b554e811572cff5ae5ee1e089684
Carlos Pinto <carlosnpinto2@gmail.com>
2015-07-05 10:43:39 +0100

    Initial implementation of player height change "menu"

---
+++ thesis_project/Assets/Scripts/Misc/HeightHandle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightHandle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
