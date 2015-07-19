1ec756626e36917b6ab88c34b6ebb2928f2edd78
Carlos Pinto <carlosnpinto2@gmail.com>
2015-07-19 17:07:33 +0100

    Restructured folders. Added logic for movement if not on menu only (except players head).

--- thesis_project/Assets/Scripts/Menu/MenuManager.cs
+++ thesis_project/Assets/Scripts/Menu/MenuManager.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 
-public class MenuManager{
+public class MenuManager {
 
 	private static MenuManager _instance = null;
 	public enum Menu {user_preferences, furniture_select, furniture_properties, none};
-	private static Menu activatedMenu = Menu.user_preferences;
+	private static Menu activatedMenu = Menu.none;
 
+	private MenuManager() {
 
-	private MenuManager()
-	{
 	}
 
-	public static MenuManager Instance
-	{
+	public static MenuManager Instance {
 		get
 		{
 			if(_instance == null)
