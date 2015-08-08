c42073a162521fd88a9bd1a60e54d15e3dcea9d1
Carlos Pinto <carlosnpinto2@gmail.com>
2015-08-08 18:53:56 +0100

    Fixed a bug of not applying the correct script to gameobject. Updated selection menu logic.

--- thesis_project/Assets/Scripts/Menu/SelectionMenu.cs
+++ thesis_project/Assets/Scripts/Menu/SelectionMenu.cs
@@ -6,7 +6,9 @@ public class SelectionMenu : MonoBehaviour {
 	private EditorGameManager egm;
 	private MenuManager mm;
 	private int selectedFurnitureIndex;
-	private float distanceToFurnitureSelect = 0.5f;
+	private float distanceToFurnitureSelect = 1.5f;
+	private bool drawnMenu = false;
+	private Vector3 furnitureSelectPos = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -50,7 +52,8 @@ public class SelectionMenu : MonoBehaviour {
 	}
 
 	private void ShowSelectionMenu (bool isToShow) {
-		if (isToShow) {
+		Debug.Log("show selection menu: " + (isToShow && mm.CanDrawMenu && !drawnMenu) );
+		if (isToShow && mm.CanDrawMenu && !drawnMenu) {
 			//Show selection menu
 			DrawSelectionMenu(true);
 		} else if (mm.ActivatedMenu == MenuManager.Menu.furniture_select) {
@@ -63,16 +66,29 @@ public class SelectionMenu : MonoBehaviour {
 		GameObject trigger = GameObject.Find("Menu_Trigger");
 
 		if (toDraw) {
+			Vector3 furniturePos;
+
+			furniturePos = drawnMenu ? furnitureSelectPos : trigger.transform.position;
+
+			//clean previous object
+			CleanPreviousFurniture();
+
 			//draw selection menu
 			GameObject furniture = (GameObject) Resources.Load("Furniture/" + (selectedFurnitureIndex + 1) + "/" + 	(selectedFurnitureIndex + 1), typeof(GameObject));
 			furniture.transform.tag = "select";
-			Instantiate(furniture, trigger.transform.position, Quaternion.identity);
-			DrawArrows(furniture.transform.position);
+
+			GameObject instantiatedFurniture = (GameObject) Instantiate(furniture, furniturePos, Quaternion.identity);
+			furnitureSelectPos = instantiatedFurniture.transform.position;
+			DrawArrows(instantiatedFurniture.transform.position);
 			
 			mm.ActivatedMenu = MenuManager.Menu.furniture_select;
+			drawnMenu = true;
 		} else {
 			//hide selection menu
+			HideMenu();
+
 			mm.ActivatedMenu = MenuManager.Menu.none;
+			drawnMenu = false;
 		}
 	}
 
@@ -83,13 +99,32 @@ public class SelectionMenu : MonoBehaviour {
 		if(rightArrow == null) {
 			rightArrow = (GameObject) Resources.Load("Arrows/RightArrow", typeof(GameObject));
 			rightArrow.transform.tag = "interactable";
-			Instantiate(rightArrow, new Vector3(centerPosition.x + distanceToFurnitureSelect, centerPosition.y, centerPosition.z), Quaternion.identity);
+			Instantiate(rightArrow, new Vector3(centerPosition.x - distanceToFurnitureSelect, centerPosition.y, centerPosition.z), Quaternion.identity);
 		}
 
 		if(leftArrow == null) {
 			leftArrow = (GameObject) Resources.Load("Arrows/LeftArrow", typeof(GameObject));
 			leftArrow.transform.tag = "interactable";
-			Instantiate(leftArrow, new Vector3(centerPosition.x - distanceToFurnitureSelect, centerPosition.y, centerPosition.z), Quaternion.identity);
+			Instantiate(leftArrow, new Vector3(centerPosition.x + distanceToFurnitureSelect, centerPosition.y, centerPosition.z), Quaternion.identity);
 		}
+	}
+
+	private void CleanPreviousFurniture() {
+		GameObject[] selectFurnitures = GameObject.FindGameObjectsWithTag("select");
+		foreach(GameObject selectFurniture in selectFurnitures){
+			Destroy(selectFurniture);
+		}
+	}
+
+	private void CleanPreviousArrows() {
+		GameObject[] arrows = GameObject.FindGameObjectsWithTag("interactable");
+		foreach(GameObject arrow in arrows){
+			Destroy(arrow);
+		}
+	}
+
+	private void HideMenu() {
+		CleanPreviousArrows();
+		CleanPreviousFurniture();
 	}
 }
