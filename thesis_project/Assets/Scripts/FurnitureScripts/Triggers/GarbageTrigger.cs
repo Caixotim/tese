780d4eaad0f0cae32c382b0dbc48346989955a17
Carlos Pinto <carlosnpinto2@gmail.com>
2015-08-09 22:13:45 +0100

    Added garbage logic.

--- thesis_project/Assets/Scripts/FurnitureScripts/Triggers/GarbageTrigger.cs
+++ thesis_project/Assets/Scripts/FurnitureScripts/Triggers/GarbageTrigger.cs
@@ -6,6 +6,9 @@ public class GarbageTrigger : MonoBehaviour {
 	void OnTriggerExit(Collider other) {
     	if (other.gameObject.tag.Equals("furniture")) {
     		//create garbage can
+    		Vector3 garbagePos = new Vector3 (this.gameObject.transform.position.x, 0.63f, this.gameObject.transform.position.z);
+    		GameObject garbageCan = (GameObject) Resources.Load("Garbage/trash", typeof(GameObject));
+    		Instantiate(garbageCan, garbagePos , Quaternion.identity);
     	}
     }
 }
