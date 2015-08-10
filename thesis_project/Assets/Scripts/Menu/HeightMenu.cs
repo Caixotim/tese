dab731ac1f42d972fe09ce15171569cf9c24655a
Carlos Pinto <carlosnpinto2@gmail.com>
2015-08-10 22:19:42 +0100

    Added script for height menu.

---
+++ thesis_project/Assets/Scripts/Menu/HeightMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
